deab719dca041e7519f05cd9723cd09c25cd2e4c
Das2127 <dasmondweijie@gmail.com>
2025-10-24 18:59:56 +0800

    Update trends_controller.rb

--- app/app/controllers/trends_controller.rb
+++ app/app/controllers/trends_controller.rb
@@ -18,7 +18,7 @@ class TrendsController < ApplicationController
         gross_monthly_median: p.gross_monthly_median.to_f,
         gross_mthly_25_percentile: p.gross_mthly_25_percentile.to_f,
         gross_mthly_75_percentile: p.gross_mthly_75_percentile.to_f,
-        #course_duration: p.course_duration.to_f
+        # course_duration: p.course_duration.to_f
       }
     end
 
